4ede9d4750cd9e4befb945ddedcdbccb32a2e18f
WilhelmKaiser <ziyang.li.nk@gmail.com>
2014-11-03 14:42:05 +0100

    update algo for chars

--- ue3/core.py
+++ ue3/core.py
@@ -8,41 +8,48 @@ from numpy import fabs
 languages = ['English', 'German_Deutsch', 'French_Francais']
 language_base = dict((language, udhr.words(language + '-Latin1')) for language in languages)
 
+# Uebung 3.1.a
 def build_language_models(languages, language_base):
     condition = [(language, char.lower()) 
                  for language in languages for word in language_base[language] for char in word]
     return ConditionalFreqDist(condition)
 
-def caculate_mann_value(lm_cfd, words):
+# Uebung 3.1.b
+def caculate_manhattan_value_char(lm_cfd, words):
     ascii = 'qwertyuiopasdfghjklzxcvbnm'
-    sorted_dict = {lang: [w for (w, _) in lm_cfd[lang].most_common() if w in ascii] for lang in lm_cfd.keys()}
-    print sorted_dict
-    sorted_freq_list = [w for (w, _) in FreqDist([c for word in words for c in word.lower()]).most_common() if w in ascii]
-    print sorted_freq_list
+    ignores = '.,-`\'""0123456789;?!:'
+    sorted_dict = {lang: [w for (w, _) in lm_cfd[lang].most_common(20) if w not in ignores] for lang in lm_cfd.keys()}
+    sorted_freq_list = [w for (w, _) in FreqDist([c for word in words for c in word.lower()]).most_common(20) if w not in ignores]
     manhattan_dict = {}
     for lang in sorted_dict.keys():
         distance = 0
-        for item in sorted_freq_list:
+        for item in sorted_dict[lang]:
             try:
-                distance += fabs(sorted_freq_list.index(item) - sorted_dict[lang].index(item))
+                distance += float(fabs(sorted_freq_list.index(item) - sorted_dict[lang].index(item))) / \
+                            len(sorted_dict[lang]) * \
+                            float(float(len(sorted_dict[lang]) - sorted_dict[lang].index(item)) / len(sorted_dict[lang]))
             except:
-                distance += fabs(sorted_freq_list.index(item) - len(sorted_dict[lang]))
-        manhattan_dict[lang] = distance
+                distance += float(fabs(sorted_dict[lang].index(item) - len(sorted_freq_list))) / \
+                            float(len(sorted_dict[lang])) / \
+                            float(float(len(sorted_dict[lang]) - sorted_dict[lang].index(item)) / len(sorted_dict[lang]))
+        manhattan_dict[lang] = distance / len(sorted_freq_list)
     return sorted(manhattan_dict.iteritems(), key=lambda item: item[1])
-    
-def guess_language(language_model_cfd, text):
+
+
+# Uebung 3.1.c
+def guess_language(language_model_cfd, text, evaluate_func=caculate_manhattan_value_char):
     test_words = word_tokenize(text)
-    print test_words
-    manhattan_distances_list = caculate_mann_value(language_model_cfd, test_words)
+    manhattan_distances_list = evaluate_func(language_model_cfd, test_words)
     print manhattan_distances_list
     return manhattan_distances_list[0][0]
     
-    
+
+# Uebung 3.1.d  
 language_model_cfd = build_language_models(languages, language_base)
 
 text1 = "these functions can return any value, which may or may not be interpretable as a Boolean value. See Comparisons for more information about rich comparisons."
 text2 = "Il dispute ses premiers matchs au sein de la Ligue d'Alsace de football, atteint la DH en 1924 et obtient le statut professionnel "
-text3 = "Sie mchten die Attraktivitt Ihres Internet-Portales erhhen und Ihren Kunden DB-Fahrkarten rund um die Uhr anbieten? Sie mchten Ihr Produktportfolio um Services rund um die Bahn erweitern? Dann arbeiten Sie mit der Deutschen Bahn zusammen und profitieren Sie von unseren attraktiven Angeboten!"
+text3 = "Sie moechten die Attraktivitaet Ihres Internet-Portales erhoehen und Ihren Kunden DB-Fahrkarten rund um die Uhr anbieten? Sie moechten Ihr Produktportfolio um Services rund um die Bahn erweitern? Dann arbeiten Sie mit der Deutschen Bahn zusammen und profitieren Sie von unseren attraktiven Angeboten!"
 
 # guess the language by comparing the frequency distributions
 print 'guess for english text is', guess_language(language_model_cfd, text1)
