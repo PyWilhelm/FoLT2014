a0fce179dc8e24c640b010717eb89e130abb2257
WilhelmKaiser <ziyang.li.nk@gmail.com>
2014-11-03 12:05:10 +0100

    test update

--- ue3/core.py
+++ ue3/core.py
@@ -1,39 +1,48 @@
 from nltk.corpus import udhr
-from nltk.probability import FreqDist
+from nltk.probability import FreqDist, ConditionalFreqDist
 from nltk import word_tokenize
 import string
+from numpy import fabs
+
 
 languages = ['English', 'German_Deutsch', 'French_Francais']
 language_base = dict((language, udhr.words(language + '-Latin1')) for language in languages)
 
 def build_language_models(languages, language_base):
-    return {language: FreqDist([w.lower() for w in language_base[language] 
-                                if not (len(w)<2 and w not in string.ascii_letters)]) 
-            for language in languages}
+    condition = [(language, char.lower()) 
+                 for language in languages for word in language_base[language] for char in word]
+    return ConditionalFreqDist(condition)
+
+def caculate_mann_value(lm_cfd, words):
+    ascii = 'qwertyuiopasdfghjklzxcvbnm'
+    sorted_dict = {lang: [w for (w, _) in lm_cfd[lang].most_common() if w in ascii] for lang in lm_cfd.keys()}
+    print sorted_dict
+    sorted_freq_list = [w for (w, _) in FreqDist([c for word in words for c in word.lower()]).most_common() if w in ascii]
+    print sorted_freq_list
+    manhattan_dict = {}
+    for lang in sorted_dict.keys():
+        distance = 0
+        for item in sorted_freq_list:
+            try:
+                distance += fabs(sorted_freq_list.index(item) - sorted_dict[lang].index(item))
+            except:
+                distance += fabs(sorted_freq_list.index(item) - len(sorted_dict[lang]))
+        manhattan_dict[lang] = distance
+    return sorted(manhattan_dict.iteritems(), key=lambda item: item[1])
     
 def guess_language(language_model_cfd, text):
     test_words = word_tokenize(text)
     print test_words
-    match_times = {l: 0 for l in language_model_cfd.keys()}
-    for language in language_model_cfd.keys():
-        for word in test_words:
-            keywords = language_model_cfd[language].keys()
-            if word.lower() in keywords:
-                print word
-                match_times[language] += 1
-    print match_times
-    max_res = ('', 0)
-    for key in match_times.keys():
-        if max_res[1] < match_times[key]:
-            max_res = (key, match_times[key])
-    return max_res[0]
+    manhattan_distances_list = caculate_mann_value(language_model_cfd, test_words)
+    print manhattan_distances_list
+    return manhattan_distances_list[0][0]
     
     
 language_model_cfd = build_language_models(languages, language_base)
 
-text1 = "Peter had been to the office before they arrived."
-text2 = "Si tu finis tes devoirs, je te donnerai des bonbons."
-text3 = "Das ist ein schon recht langes deutsches Beispiel."
+text1 = "these functions can return any value, which may or may not be interpretable as a Boolean value. See Comparisons for more information about rich comparisons."
+text2 = "Il dispute ses premiers matchs au sein de la Ligue d'Alsace de football, atteint la DH en 1924 et obtient le statut professionnel "
+text3 = "Sie mchten die Attraktivitt Ihres Internet-Portales erhhen und Ihren Kunden DB-Fahrkarten rund um die Uhr anbieten? Sie mchten Ihr Produktportfolio um Services rund um die Bahn erweitern? Dann arbeiten Sie mit der Deutschen Bahn zusammen und profitieren Sie von unseren attraktiven Angeboten!"
 
 # guess the language by comparing the frequency distributions
 print 'guess for english text is', guess_language(language_model_cfd, text1)
